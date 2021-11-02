8ad62c223c7a7ccff08b389cecb97fca25e92b57
Antoan Petrov <72857425+AntoanPetrov123@users.noreply.github.com>
2021-11-02 11:33:49 +0200

    Delete 3.TownInfo.js

--- 3.TownInfo.js
+++
@@ -1,6 +0,0 @@
-function townInfo(town, population, area){
-
-    let result = `Town ${town} has population of ${population} and area ${area} square km.`
-    console.log(result);
-}
-townInfo('Sofia', 1286383, 492)
